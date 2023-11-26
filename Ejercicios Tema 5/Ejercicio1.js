3fd5f25f78214d6047670a1df8aa0a63b1dbd9c3
dagumo92 <dagumo92@hotmail.com>
2023-11-26 17:08:17 +0100

    añadimos linea para que vaya prompt

--- Ejercicios Tema 5/Ejercicio1.js
+++ Ejercicios Tema 5/Ejercicio1.js
@@ -1,3 +1,4 @@
-var base= parseInt(prompt("dime la base del triangulo"));
-var altura= parseInt(prompt("dime la altura del triangulo"));
-console.log("el area del triangulo es:" + ((base * atura)/2));
+const prompt = require('prompt-sync')({sigint: true});
+var base= parseInt(prompt("dime la base del triangulo: "));
+var altura= parseInt(prompt("dime la altura del triangulo: "));
+console.log("el area del triangulo es:" + ((base * altura)/2));
