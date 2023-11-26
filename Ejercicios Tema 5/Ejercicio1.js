2d3385a7575d80b5ddc89e002c25b72d64fd1c36
dagumo92 <dagumo92@hotmail.com>
2023-11-26 16:46:10 +0100

    organización

---
+++ Ejercicios Tema 5/Ejercicio1.js
@@ -0,0 +1,3 @@
+var base= parseInt(prompt("dime la base del triangulo"));
+var altura= parseInt(prompt("dime la altura del triangulo"));
+console.log("el area del triangulo es:" + ((base * atura)/2));
